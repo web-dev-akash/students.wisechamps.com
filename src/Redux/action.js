febd26219bd791d0baac1523b96033844316974a
web-dev-akash <akumar415129.ak@gmail.com>
2024-05-03 16:11:15 +0530

    Email Validation impoved

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -11,7 +11,7 @@ import {
 } from "./actionTypes";
 
 const emailRegex = new RegExp(
-  /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/,
+  /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
   "gm"
 );
 
